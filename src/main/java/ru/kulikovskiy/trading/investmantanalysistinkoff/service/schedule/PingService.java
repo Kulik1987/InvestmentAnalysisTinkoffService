f09cc2b68ea3cb7afa1fc0ede04b71ecb6c5abf4
Kulik1987 <kulik7771@ya.ru>
2021-03-30 10:05:57 +0300

    правка application.yml

---
+++ src/main/java/ru/kulikovskiy/trading/investmantanalysistinkoff/service/schedule/PingService.java
@@ -0,0 +1,5 @@
+package ru.kulikovskiy.trading.investmantanalysistinkoff.service.schedule;
+
+public interface PingService {
+    void pingBot();
+}
---
+++ src/main/java/ru/kulikovskiy/trading/investmantanalysistinkoff/service/schedule/PingServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.kulikovskiy.trading.investmantanalysistinkoff.service.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.kulikovskiy.trading.investmantanalysistinkoff.exception.NotFoundException;
+import ru.kulikovskiy.trading.investmantanalysistinkoff.service.AccountService;
+
+@Service
+@Slf4j
+public class PingServiceImpl implements PingService {
+    @Autowired
+    private AccountService accountService;
+
+    @Override
+    @Scheduled(cron = "0 0 1-2 * * *")
+    public void pingBot() {
+        try {
+            String token = accountService.getToken("1");
+        } catch (NotFoundException e) {
+            log.info("app don't sleep");
+        }
+    }
+}
